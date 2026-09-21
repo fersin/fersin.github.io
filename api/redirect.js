eefe5c25d8c1943a689dc37d82165ef68186316f
fersin <79020501+fersin@users.noreply.github.com>
2026-09-21 11:27:44 -0600

    Create redirect.js

---
+++ api/redirect.js
@@ -0,0 +1,7 @@
+// api/redirect.js
+export default function handler (req, res) {
+  const target = "http://localhost:5000/ethiack_1337_lmao"
+
+  res.writehead(302, { location: target});
+  res.end();
+}
